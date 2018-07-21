3d9c811e60f5830a1f86e7109429d63bb3ff3c67
Lucy Wall <lhwall@gmail.com>
2018-07-21 15:28:57 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,22 @@ def new
 end
 
 def create
-  byebug
+  #byebug
   if session[:name]
-    redirect_to controller: "application", action: "hello"
+    redirect_to controller: 'application', action: 'hello'
+  elsif !params[:name] || params[:name].empty?
+    redirect_to login_url
   else
   session[:name] = params[:name]
-  redirect_to :action => "new"
+  redirect_to controller: 'application', action: 'hello'
 end
 end
 
+
+
 def destroy
+  session.delete :name
+  redirect_to :action => "create"
 end
 
 end
